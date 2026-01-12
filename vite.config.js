1cbbda3d9056b38ff4b8d179cfba036e3f3041b0
L3_V055 <mariovoss08@gmail.com>
2026-01-13 00:16:23 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets', // Définit le sous-dossier pour les assets
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  // Pour référencer correctement les assets
+  base: './',
+  publicDir: 'public'
 })
